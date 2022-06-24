6f21912a84a084107e9e84af56c9b3a51dee5182
ruhao <13817892631@163.com>
2022-06-24 13:48:53 +0800

    test

---
+++ src/views/About.vue
@@ -0,0 +1,16 @@
+<!--
+ * @Author: ruhao 13817892631@163.com
+ * @Date: 2022-06-24 12:36:27
+ * @LastEditors: ruhao 13817892631@163.com
+ * @LastEditTime: 2022-06-24 13:48:07
+ * @FilePath: /vuedemo/src/views/About.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+
+
+    
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<!--
+ * @Author: ruhao 13817892631@163.com
+ * @Date: 2022-06-24 12:36:27
+ * @LastEditors: ruhao 13817892631@163.com
+ * @LastEditTime: 2022-06-24 13:41:27
+ * @FilePath: /vuedemo/src/views/Home.vue
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+-->
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+})
+</script>
