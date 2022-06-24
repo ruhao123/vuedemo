899044b2db0f18698a64c3cb46aa85655629cfdd
ruhao <13817892631@163.com>
2022-06-24 13:54:48 +0800

    new

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
  * @Author: ruhao 13817892631@163.com
  * @Date: 2022-06-24 12:36:27
  * @LastEditors: ruhao 13817892631@163.com
- * @LastEditTime: 2022-06-24 13:41:27
+ * @LastEditTime: 2022-06-24 13:54:28
  * @FilePath: /vuedemo/src/views/Home.vue
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
@@ -21,6 +21,11 @@ export default defineComponent({
   name: 'Home',
   components: {
     HelloWorld
+
+
+
+
+    
   }
 })
 </script>
